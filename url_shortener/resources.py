f3c87d11722580928280c3323ac39f5f6456d988
André Felipe Dias <andre.dias@pronus.io>
2023-03-30 00:03:14 -0300

    remove unnecessary loguru configuration

--- url_shortener/resources.py
+++ url_shortener/resources.py
@@ -1,7 +1,3 @@
-import logging
-import sys
-from string import ascii_uppercase
-
 from databases import Database
 from loguru import logger
 from sqlalchemy import create_engine
@@ -14,7 +10,6 @@ db = Database(config.DATABASE_URL, force_rollback=config.TESTING)
 
 
 async def startup() -> None:
-    setup_logger()
     show_config()
     await start_database()
     logger.info('started...')
@@ -25,56 +20,9 @@ async def shutdown() -> None:
     logger.info('...shutdown')
 
 
-def setup_logger() -> None:
-    """
-    Configure Loguru's logger
-    """
-    _intercept_standard_logging_messages()
-    logger.remove()  # remove standard handler
-    logger.add(
-        sys.stderr,
-        level=config.LOG_LEVEL,
-        colorize=True,
-        backtrace=config.DEBUG,
-        enqueue=True,
-    )  # reinsert it to make it run in a different thread
-
-
-def _intercept_standard_logging_messages() -> None:
-    """
-    Intercept standard logging messages toward loguru's logger
-    ref: https://github.com/Delgan/loguru#entirely-compatible-with-standard-logging  # noqa: E501
-    """
-
-    class InterceptHandler(logging.Handler):
-        def emit(self, record: logging.LogRecord) -> None:
-            # Get corresponding Loguru level if it exists
-            try:
-                level = logger.level(record.levelname).no
-            except ValueError:
-                level = record.levelno
-
-            # Find caller from where originated the logged message
-            frame, depth = logging.currentframe(), 2
-            while frame.f_code.co_filename == logging.__file__:
-                frame = frame.f_back  # type: ignore
-                depth += 1
-
-            logger.opt(depth=depth, exception=record.exc_info).log(
-                level, record.getMessage()
-            )
-
-    logging.basicConfig(handlers=[InterceptHandler()], level=0, force=True)
-
-
 def show_config() -> None:
-    config_vars = {
-        key: getattr(config, key)
-        for key in sorted(dir(config))
-        if key[0] in ascii_uppercase
-    }
+    config_vars = {key: getattr(config, key) for key in sorted(dir(config)) if key.isupper()}
     logger.debug(config_vars)
-    return
 
 
 async def connect_database(database: Database) -> None:
@@ -98,4 +46,3 @@ def create_db() -> None:
 async def start_database() -> None:
     await connect_database(db)
     create_db()
-    # migrate_db()
